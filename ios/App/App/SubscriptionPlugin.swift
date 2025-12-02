fab12a1ca6b01b64a219a8ea00bb51bc2b56dc9f
faisalarip <faisalarip10@gmail.com>
2025-12-02 23:00:06 +0700

    fix conflicts

--- ios/App/App/SubscriptionPlugin.swift
+++ ios/App/App/SubscriptionPlugin.swift
@@ -3,7 +3,6 @@ import Capacitor
 import StoreKit
 
 @objc(SubscriptionPlugin)
-<<<<<<< HEAD
 public class SubscriptionPlugin: CAPPlugin, CAPBridgedPlugin {
     
     public let identifier = "SubscriptionPlugin"
@@ -13,12 +12,6 @@ public class SubscriptionPlugin: CAPPlugin, CAPBridgedPlugin {
         CAPPluginMethod(name: "purchase", returnType: CAPPluginReturnPromise),
         CAPPluginMethod(name: "restore", returnType: CAPPluginReturnPromise)
     ]
-=======
-public class SubscriptionPlugin: CAPPlugin {
-    public override func load() {
-        NSLog("[SubscriptionPlugin] loaded")
-    }
->>>>>>> ed0172fef807a28157014e8bbfb119f29d86259c
     
     @objc public func getProducts(_ call: CAPPluginCall) {
         guard let ids = call.getArray("ids", String.self), !ids.isEmpty else {
